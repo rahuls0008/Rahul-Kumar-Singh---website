48c2bef0f9c1d771b12e355fbb545e637b994cf5
rahuls0008 <rahulksingh0008@gmail.com>
2021-11-29 21:34:45 +0530

    Auto-Fill Skill Bar when Skills section appear on screen

--- script.js
+++ script.js
@@ -77,3 +77,67 @@ function scrollVertically(targetSection){                  // pass targetSection
 	}
 	window.scrollBy(0, 60);      //scroll by 50px
 }
+
+
+
+
+
+// Handle scroll event on Window 
+// Check that skills sections container is currently visible or not
+// Ensure that initial width of colored skill divs is Zero --> intiliased/reset to 0 width value
+// As soon as it is visible Start animation on every skill --> increase skill widh from 0 to skill level
+// Store skill level --> HTML with the help data attribute
+
+
+var progressBars = document.querySelectorAll('.skill-progress > div');   // Fetch all the Progress Bars [outer div]
+var skillsContainer = document.getElementById('skills-container'); 
+window.addEventListener('scroll', checkScroll);
+
+var animationDone = false;
+
+//Initially Filled 
+initialiseBars();     // Make it empty & then Fill them to create  animation
+
+function initialiseBars() {
+	//for-of loop to traverse progressBars
+	for(let bar  of progressBars){
+		bar.style.width = 0 + '%';
+	}
+}
+    
+
+       // ** NOT VAR because we want different width for each iteration  LET IS IMPORTANT **//
+function fillBars(){
+	for(let bar of progressBars){
+		let targetWidth = bar.getAttribute('data-bar-width');             // ** NOT VAR because we want different width for each iteration  **//
+		let currentWidth = 0;
+		let interval = setInterval(function(){
+			if(currentWidth > targetWidth){
+				clearInterval(interval);
+				return;
+			}
+
+			currentWidth++;
+			bar.style.width = currentWidth + '%';     //update the width of the bar
+		},15);
+	}
+}
+
+function checkScroll(){
+	//Check whether skill-container is on the screen
+	var skillCoordinates = skillsContainer.getBoundingClientRect();
+
+	if(!animationDone && skillCoordinates.top < window.innerHeight){          // window.innerHeight  -> viewport height   i.e skills section is visible on the screen      
+		
+		animationDone = true;       //to make animation take place only once
+		console.log('Skills Section is visible on the screen'); 
+
+		fillBars(); 
+	
+	}else if(skillCoordinates.top > window.innerHeight){    
+		animationDone = false;
+		initialiseBars();  
+	}
+}
+
+
